ed1be62b3a9d9c3bfcb6076ea63e2f29568f2754
jakub-wojciechowski <jakub@alice.si>
2018-03-06 21:14:59 +0100

    Integration Pension Fund deployment

--- test/integration/Investment.test.js
+++ test/integration/Investment.test.js
@@ -4,6 +4,9 @@ const Moment = require('moment');
 
 const User = artifacts.require('./User.sol');
 const UserRegistry = artifacts.require('./UserRegistry.sol');
+const FundRegistry = artifacts.require('./PensionFundsRegistry.sol');
+const PensionFund = artifacts.require('./PensionFund.sol');
+const AkropolisToken = artifacts.require('./AkropolisToken.sol');
 
 const BigNumber = web3.BigNumber;
 
@@ -12,22 +15,35 @@ const should = require('chai')
 	.use(require('chai-bignumber')(BigNumber))
 	.should();
 
-contract('Investment', function ([owner, userAccount]) {
+contract('Investment', function ([owner, userAccount, fundAccount]) {
 
 	const DOB = Moment("1983-09-19");
 
-	let registry, user;
+	let userRegistry, user, fundRegistry, fund, aet;
 
 	before(async function () {
-		registry = await UserRegistry.deployed();
+		userRegistry = await UserRegistry.deployed();
+		fundRegistry = await FundRegistry.deployed();
+		aet = await AkropolisToken.deployed();
 	});
 
 
 	it('should create a user', async function () {
-		await registry.createUser(DOB.unix(), {from: userAccount});
-		user = User.at(await registry.getUserContract(userAccount));
+		await userRegistry.createUser(DOB.unix(), {from: userAccount});
+		user = User.at(await userRegistry.getUserContract(userAccount));
 
 		(await user.dateOfBirth()).should.be.bignumber.equal(DOB.unix());
+		(await user.owner()).should.be.equal(userAccount);
+	});
+
+
+	it('should create a pension fund', async function () {
+		await aet.mint(fundAccount, 100, {from: owner});
+		await aet.approve(fundRegistry.address, 100, {from: fundAccount});
+		await fundRegistry.createAndRegisterPensionFund("FUND", {from: fundAccount});
+		fund = PensionFund.at(await fundRegistry.getFund("FUND"));
+
+		(await fund.owner()).should.be.equal(fundAccount);
 	});
 
 
