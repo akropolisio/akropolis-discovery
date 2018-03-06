4c34a7e2427529d8f9e29603f0047beebe025c2c
jakub-wojciechowski <jakub@alice.si>
2018-03-06 20:41:47 +0100

    Rename deployment scripts

--- migrations/5_deploy_pension_fund_registry.js
+++ migrations/5_deploy_pension_fund_registry.js
@@ -4,4 +4,6 @@ const StakingPool = artifacts.require('./StakingPool.sol');
 
 module.exports = async function(deployer) {
 	await deployer.deploy(PensionFundsRegistry, AkropolisToken.address, StakingPool.address);
+	var registry = await PensionFundsRegistry.deployed();
+	await registry.setMinStake(100);
 };
