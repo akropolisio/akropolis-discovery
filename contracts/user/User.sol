12e520bab64b8f8e4d8fbc90a055beafe8225d2a
jakub-wojciechowski <jakub@alice.si>
2018-03-07 20:52:36 +0100

    Opening a saving account.

--- contracts/user/User.sol
+++ contracts/user/User.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import './Wallet.sol';
+import './SavingsAccount.sol';
+import '../network/PensionFundsRegistry.sol';
 
 
 /**
@@ -13,9 +15,42 @@ contract User is Ownable {
     uint256 public dateOfBirth;
     Wallet public wallet;
 
+    mapping(bytes32 => SavingsAccount) savingAccounts;
+    bytes32[] savingAccountsList;
+
     function User(uint256 _dateOfBirth, Wallet _wallet) public {
         dateOfBirth = _dateOfBirth;
         wallet = _wallet;
+    }
+
+
+    function openSavingAccount(bytes32 _name, PensionFundsRegistry _pensionFundsRegistry) public onlyOwner {
+        require(savingAccountsList.length < 16);
+        require(address(savingAccounts[_name]) == 0x0);
+        SavingsAccount account = new SavingsAccount(_pensionFundsRegistry);
+        savingAccounts[_name] = account;
+        savingAccountsList.push(_name);
+    }
+
+    function createDefaultAccounts(PensionFundsRegistry _pensionFundsRegistry) public onlyOwner {
+        openSavingAccount("VOLUNTARY", _pensionFundsRegistry);
+        openSavingAccount("EMERGENCY", _pensionFundsRegistry);
+        openSavingAccount("SHORT_TERM", _pensionFundsRegistry);
+    }
+
+
+    function getSavingAccountByName(bytes32 _name) public view returns(SavingsAccount) {
+        return savingAccounts[_name];
+    }
+
+
+    function getSavingAccountByIndex(uint256 _index) public view returns(SavingsAccount) {
+        return savingAccounts[savingAccountsList[_index]];
+    }
+
+
+    function getSavingAccountsCount() public view returns(uint256) {
+        return savingAccountsList.length;
     }
 
 }
