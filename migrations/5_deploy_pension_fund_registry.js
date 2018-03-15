bd464a731870066ecc8522af05470da91bd0cd4b
jakub-wojciechowski <jakub@alice.si>
2018-03-15 09:03:16 +0100

    Testnet deployment

--- migrations/5_deploy_pension_fund_registry.js
+++ migrations/5_deploy_pension_fund_registry.js
@@ -2,8 +2,11 @@ const PensionFundsRegistry = artifacts.require('./PensionFundsRegistry.sol');
 const AkropolisToken = artifacts.require('./AkropolisToken.sol');
 const StakingPool = artifacts.require('./StakingPool.sol');
 
-module.exports = async function(deployer) {
-	await deployer.deploy(PensionFundsRegistry, AkropolisToken.address, StakingPool.address);
-	var registry = await PensionFundsRegistry.deployed();
-	await registry.setMinStake(web3.toWei(100, "ether"));
+module.exports = function(deployer) {
+	deployer.deploy(PensionFundsRegistry, AkropolisToken.address, StakingPool.address);
+	deployer.then(function() {
+		return PensionFundsRegistry.deployed();
+	}).then(function(instance) {
+		return instance.setMinStake(web3.toWei(100, "ether"));
+	});
 };
--- truffle.js
+++ truffle.js
@@ -1,6 +1,9 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+//TODO: For testing only, update and dont commit when moving to public demo
+const config = require('./test-config.json');
 
 module.exports = {
 	networks: {
@@ -15,6 +18,14 @@ module.exports = {
 			port: 8555,
 			gas: 0xfffffffffff, // <-- Use this high gas value
 			gasPrice: 0x01      // <-- Use this low gas price
+		},
+		local_kovan: {
+			provider: function() {
+				return new HDWalletProvider(config.mnemonic, "http://localhost:8545")
+			},
+			network_id: 42,
+			gas: 5000000, // <-- Use this high gas value
+			gasPrice: 0x05
 		},
 		solc: {
 			optimizer: {
